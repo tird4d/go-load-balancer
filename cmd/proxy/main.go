7b46173a9752faf0e985d9ef5b8c97177bfdb56d
Tirdad Abbasi <abbasi.tirdad@gmail.com>
2026-04-20 00:58:20 +0200

    Add initial implementation of TCP proxy and goroutines example

---
+++ cmd/proxy/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+)
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+	fmt.Printf("The connection accepted. %s", conn.RemoteAddr())
+
+	buf := make([]byte, 1024)
+	for {
+		conn.Read(buf)
+		fmt.Println(string(buf))
+	}
+
+}
+
+func main() {
+
+	address := ":8080"
+
+	listener, err := net.Listen("tcp", address)
+	if err != nil {
+		log.Fatal("Listen error", err)
+	}
+
+	defer listener.Close()
+
+	log.Printf("Server is ready on the Port %s", address)
+
+	conn, err := listener.Accept()
+
+	if err != nil {
+		log.Fatal("connection error")
+	}
+
+	handleConnection(conn)
+
+}
